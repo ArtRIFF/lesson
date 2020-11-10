0b7096acad472d1c30a07dbc4c6459875e9c5796
ArtRIFF <panasuck@bigmir.net>
2020-11-10 19:52:06 +0200

    object

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 "use strict";
-alert("its js work!");
+const numberOfFilms=+prompt('Сколько фильмов посмотрел?');
+const personalMovieDB = {
+count:numberOfFilms,
+movies:{},
+actors:{},
+genres:[],
+privat:false
+};
+
+ const a=prompt('Последний фильм?');
+ const b=prompt('На сколько оцените  фильм?');
+ const c=prompt('Последний фильм?');
+ const d=prompt('На сколько оцените  фильм?');
+
+ personalMovieDB.movies[a]=b;
+ personalMovieDB.movies[c]=d;
+ console.log(personalMovieDB);
